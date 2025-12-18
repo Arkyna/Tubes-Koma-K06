ab80f2cd52fb7060c71819a1227e2d1d1810e5af
Arkyna <160804616+Arkyna@users.noreply.github.com>
2025-12-18 18:35:00 +0700

    feat: update allowed origins in config.py; replace production frontend URL with custom domain URLs

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,11 @@
-PRODUCTION_FRONTEND_URL = "https://frontend-service-255243454378.asia-southeast2.run.app"
-
+CUSTOM_DOMAIN_URL = "https://www.facilitywatch-174.my.id"
+CUSTOM_DOMAIN_ROOT = "https://facilitywatch-174.my.id"
 
 # Daftar Whitelist
 ALLOWED_ORIGINS = [
     "http://localhost:5500",      # Local Development
     "http://127.0.0.1:5500",      # Local IP
     "http://localhost:8000",      # Swagger UI Local
-    PRODUCTION_FRONTEND_URL       # Production Cloud Run
+    CUSTOM_DOMAIN_URL,
+    CUSTOM_DOMAIN_ROOT            # Production Cloud Run
 ]
